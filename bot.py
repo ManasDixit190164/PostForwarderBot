b71235945b28dcf657285c592512c67b5ffc6958
M4nfy <92004840+M4nfy@users.noreply.github.com>
2021-10-31 21:42:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,16 +12,17 @@ API_ID = environ.get('API_ID', '6')
 API_HASH = environ.get('API_HASH', 'eb06d4abfb49dc3eeb1aeb98ae0f581e')
 BOT_TOKEN = '2013999702:AAFKYOfzAjVE5k4xKwntjMNFzdXNbKeiZas'
 from_channel = -1001385076818
-to_channel = [-1001797223248,-1001747779397,-1001663802984,-1001592836028,-1001529949759,-1001526995634,-1001525770536,-1001333978413,-1001239074731,-1001219612175,-1001346637937,-1001436098326,-1001372997271,-1001497325726,-1001457625038,-1001748493882,-1001593784776,-1001155233048,-1001187925614,-1001368695613,-1001445095452,-1001524026545,-1001538321307,-1001572164185,-1001517048806]
+to_channel = [-1001797223248,-1001747779397,-1001663802984,-1001592836028,-1001529949759,-1001526995634,-1001525770536,-1001333978413,-1001239074731,-1001219612175,-1001436098326,-1001372997271,-1001497325726,-1001457625038,-1001748493882,-1001593784776,-1001155233048,-1001187925614,-1001368695613,-1001445095452,-1001524026545,-1001538321307,-1001572164185,-1001517048806,-1001582900944,-1001480873177,-1001579175259]
 
 bot = TelegramClient('bot', api_id=API_ID, api_hash=API_HASH).start(bot_token=BOT_TOKEN)
 
 
 @bot.on(events.NewMessage(incoming=True, chats=from_channel))
 async def _(event):
-    if not event.is_private:
+    #if not event.is_private:
+    for i in to_channel:
         try:
-            for i in to_channel:
+            #for i in to_channel:
                 time.sleep(0.8)
                 if event.poll:
                     return
@@ -41,7 +42,8 @@ async def _(event):
                     await bot.send_message(i, event.text, link_preview=False)
 
         except Exception as e:
-            print("ERROR : "+str(e)+str(to_channel))
+            print("ERROR : "+str(e))
+            continue
 
 print("Bot has been deployed.")
 bot.run_until_disconnected()
